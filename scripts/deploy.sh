9d219e2695c4e23415308e648c210296e949d6b1
sonyamoonglade <aalexandrovich.hireme@gmail.com>
2023-03-31 21:26:25 +0300

    update scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,23 +10,15 @@ mkdir build
 GOOS=$OS GOARCH=$ARCH go build -o ./build/app cmd/app/main.go
 cp templates.json ./build
 cp -r videos ./build
-
 echo "building..."
-
-# stop existing session
-ssh -i ~/.ssh/vadim-shop $USER@$IP "kill -9 \$(pidof app)"
-
-echo "stopped running process"
 
 # transfer build folder 
 scp -r -i ~/.ssh/vadim-shop build $USER@$IP:./
-
 echo "copying build folder"
 
-# run new version
-ssh -i ~/.ssh/vadim-shop $USER@$IP "./run.sh </dev/null >~/backup-log.log 2>&1 &" 
-
-echo "started new process"
+# stop existing session
+ssh -i ~/.ssh/vadim-shop $USER@$IP "kill -9 \$(pidof app)"
+echo "stopped running process"
 
 # cleanup
 rm -rf ./build
